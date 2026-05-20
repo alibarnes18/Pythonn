02f408ad313bfc7b2ec53e5de0062144b996a9b2
alizen1107 <aliozen1107@gmail.com>
2026-05-20 20:10:39 +0300

    Add DNS query functionality to ns02.py for A, MX, and NS records

--- Network Security/ns02.py
+++ Network Security/ns02.py
@@ -1,8 +1,33 @@
 import dns.resolver
 
-kayit_tipleri = ["A", "MX", "NS"]
-domain = "google.com"
+def dns_sorgula(domain, dosya):
 
-for tip in kayit_tipler:
-    print(f"\n{tip} Kayitlar: ")
-    sonuc = dns.resolver.resolve(domain, tip)
+    kayit_tipleri = ["A", "MX", "NS"]
+
+    with open(dosya, "w", encoding="utf-8") as rapor:
+
+
+        for tip in kayit_tipleri:
+            rapor.write(f"\n{tip} Kayıtları:\n")
+            print(f"\n{tip} Kayıtlar:")
+
+            try:
+                sonuc = dns.resolver.resolve(domain, tip)
+
+                for kayit in sonuc:
+                    satir = f" {kayit}"
+                    print(satir)
+                    rapor.write(satir + "\n")
+            
+            except Exception as e:
+                hata = f"  [!] {tip} kaydı alınamadı: {e}"
+                print(hata)
+                rapor.write(hata + "\n")  
+
+
+
+
+domain = "google.com"
+dosya = "dns_report.txt"
+
+dns_sorgula(domain, dosya)
