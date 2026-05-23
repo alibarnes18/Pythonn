21fae2835a7a1d40987ac9cd8046058bfef41adc
alizen1107 <aliozen1107@gmail.com>
2026-05-23 22:08:57 +0300

    z

--- websecurity/test_server.py
+++ websecurity/test_server.py
@@ -9,7 +9,13 @@ def search():
     if "'" in query or  "OR" in query.upper():
         return "MySql Error: You have an error in your Sql syntax", 500
     
-    return f"Sonuçlar: {query}", 200   
+    return f"Sonuçlar: {query}", 200  
+
+@app.route("/comment")
+def comment():
+    yorum = request.args.get("yorum", "")
+    # Savunmasız — input filtrelenmiyor
+    return f"<html><body>{yorum}</body></html>", 200 
 
 if __name__ == "__main__":
     app.run(port=5000)
